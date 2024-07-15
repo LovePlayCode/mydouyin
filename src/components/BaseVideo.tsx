08ddebf46e4b8aa908904f962cbd1210f64b13bf
lhl <1244224501@qq.com>
2024-07-15 23:03:09 +0800

    feat: 添加进度条功能

--- src/components/BaseVideo.tsx
+++ src/components/BaseVideo.tsx
@@ -1,6 +1,6 @@
 import { type FC, useEffect, useRef, useState } from 'react';
 import './BaseVideo.less';
-import { useMount } from 'ahooks';
+import { useMount, useSetState } from 'ahooks';
 import { IconPlayArrowFill } from '@arco-design/web-react/icon';
 import ItemToolbar from './ItemToolbar';
 import ItemDesc from './ItemDesc';
@@ -13,30 +13,70 @@ interface BaseVideoProps {
   isplay: boolean;
 }
 const BaseVideo: FC<BaseVideoProps> = ({ videoUrl, avatarUrl, isplay }) => {
-  const [status, setStatus] = useState(
-    isplay ? MediaEnum.PLAY : MediaEnum.PAUSE,
-  );
+  const [state, setState] = useSetState({
+    status: isplay ? MediaEnum.PLAY : MediaEnum.PAUSE,
+    playX: 0,
+  });
 
+  const baseVideoRef = useRef({
+    step: 0,
+    progressBarRect: {
+      height: 0,
+      width: 0,
+    },
+    duration: 0,
+    currentTime: 0,
+  });
   const videoEl = useRef<HTMLVideoElement>(null);
+  const progressEl = useRef<HTMLDivElement>(null);
+  const loadedMetadata = (e: any) => {
+    // 获取播放进度
+    const { target } = e ?? {};
+    const { current } = baseVideoRef;
+    current.duration = target.duration;
+    current.progressBarRect = progressEl.current?.getBoundingClientRect() || {
+      height: 0,
+      width: 0,
+    };
+    // 获取步数
+    current.step = current.progressBarRect.width / Math.floor(current.duration);
+  };
+  // 视频时间更新时会触发
+  const timeupdate = (e: any) => {
+    baseVideoRef.current.currentTime = Math.ceil(e.target.currentTime);
+
+    setState({
+      playX: (baseVideoRef.current.currentTime - 1) * baseVideoRef.current.step,
+    });
+    console.log(
+      'baseVideoRef==',
+      (baseVideoRef.current.currentTime - 1) * baseVideoRef.current.step,
+    );
+  };
   const play = () => {
     if (!videoEl.current) {
       console.error('播放器初始化失败,请刷新重试');
     }
     if (videoEl.current) {
       videoEl.current.volume = 1;
-      setStatus(MediaEnum.PLAY);
+      setState({
+        status: MediaEnum.PLAY,
+      });
+      // setStatus(MediaEnum.PLAY);
     }
 
     videoEl.current?.play();
   };
   const pause = () => {
-    setStatus(MediaEnum.PAUSE);
+    // setStatus(MediaEnum.PAUSE);
+    setState({
+      status: MediaEnum.PAUSE,
+    });
     videoEl.current?.pause();
   };
-  useEffect(() => {
-    console.log('播放器状态==', status);
-  }, [status]);
+
   const click = ({ type }: { type: EVENTKEYENUM }) => {
+    const { status } = state ?? {};
     if (type === EVENTKEYENUM.ITEM_TOGGLE) {
       if (status === MediaEnum.PLAY) {
         pause();
@@ -55,15 +95,30 @@ const BaseVideo: FC<BaseVideoProps> = ({ videoUrl, avatarUrl, isplay }) => {
         muted
         ref={videoEl}
         src={videoUrl}
+        onLoadedMetadata={loadedMetadata}
+        onTimeUpdate={timeupdate}
         loop
       />
-      {!(status === MediaEnum.PLAY) && (
+      {!(state.status === MediaEnum.PLAY) && (
         <IconPlayArrowFill className="pause-icon" />
       )}
       <div className="float">
         <div className="normal">
           <ItemToolbar avatarUrl={avatarUrl} />
           <ItemDesc />
+        </div>
+        {/* 进度条 */}
+        <div className="progress" ref={progressEl}>
+          {baseVideoRef.current.duration > 15 && (
+            <>
+              <div className="bg" />
+              <div
+                className="progress-line"
+                style={{ width: `${state.playX}px` }}
+              />
+              <div className="point" />
+            </>
+          )}
         </div>
       </div>
     </div>
